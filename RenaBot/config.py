d06bfa866a7f4df8e61db3d1e768cf678df42e64
Cyanidetitan <122152717+Cyanidetitan@users.noreply.github.com>
2023-09-27 22:38:02 +0530

    Update config.py

--- RenaBot/config.py
+++ RenaBot/config.py
@@ -1,7 +1,7 @@
 import os
 from telethon import TelegramClient
 
-api_id = "10247139"
+api_id = 10247139
 api_hash = "96b46175824223a33737657ab943fd6a"
 Bot_token = "6496564064:AAH-7Fi79eZz9SfDyrVNgnfVIEiG2pdJ3Oc"
 Admin = "5894098166"
