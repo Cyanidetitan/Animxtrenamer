242e22d97a23aa705edc82a8357513cbdfde72e7
Cyanidetitan <122152717+Cyanidetitan@users.noreply.github.com>
2023-09-27 23:01:30 +0530

    Update config.py

--- RenaBot/config.py
+++ RenaBot/config.py
@@ -1,8 +1,8 @@
 import os
 from telethon import TelegramClient
 
-api_id = 10247139
+api_id = "10247139"
 api_hash = "96b46175824223a33737657ab943fd6a"
-Bot_token = "5306499440:AAHpWLxsP6tFAsxZ7vEx4vqTe1HZsxThn7A"
+Bot_token = "5306499440:AAH2t1ujNGsSStCopXvt9CfBd_v5m6MFwVE"
 Admin = "5894098166"
 client=TelegramClient('bot',api_id,api_hash).start(bot_token=Bot_token)
